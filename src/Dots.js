dc33e0ab44e8ed7675e6f0a6d30c516ddbd6d2c4
Jake Taylor <me@jaketaylor.co>
2018-11-08 15:15:01 +0000

    Remove unused var

--- src/Dots.js
+++ src/Dots.js
@@ -10,13 +10,4 @@ const Dots = ({ light }) => [1, 2, 3, 4].map(n => (
     <Dot number={n} lit={n <= light} key={n} />
 ));
 
-const Dots__ = ({ light }) => (
-    <>
-        <div className="dot dot-1" />
-        <div className="dot dot-2" />
-        <div className="dot dot-3" />
-        <div className="dot dot-4" />
-    </>
-);
-
 export default Dots;
